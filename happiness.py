4051e253163b155c021694a4964798f87db7ea0c
tarotez <tez@sings.jp>
2019-10-16 20:11:19 +0900

    happiness

---
+++ happiness.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+d = pd.read_csv('2017.csv')
+happiness = np.array(d['Happiness.Score'])
+gdp = np.array(d['Economy..GDP.per.Capita.'])
+family = np.array(d['Family'])
+plt.scatter(gdp,happiness)
+plt.xlabel('GDP', fontsize=18)
+plt.ylabel('Happiness', fontsize=18)
