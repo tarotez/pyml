761fde4d306167149d246ed9ca7a5459b445df58
tarotez <tez@sings.jp>
2019-10-18 18:30:52 +0900

    seq2seq

--- seq2seq.py
+++ seq2seq.py
@@ -5,7 +5,7 @@ from keras.layers import Input, LSTM, Dense
 import numpy as np
 
 batch_size = 64  # Batch size for training.
-epochs = 20  # Number of epochs to train for.
+epochs = 10  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
