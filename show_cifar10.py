b35066875fa969e736fd8cbb3aad128cadb09e97
tarotez <tez@sings.jp>
2019-10-18 12:14:28 +0900

    show images

--- show_cifar10.py
+++ show_cifar10.py
@@ -3,7 +3,6 @@ import numpy as np
 from keras.datasets import cifar10
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
 imgs = []
-for i in range(100):
-	a = X_train[i,:,:,:]
-	t = np.transpose(a,(1,2,0))
-	imgs.append(Image.fromarray(np.uint8(t)))
+i = 5
+a = X_train[i,:,:,:]
+Image.fromarray(np.uint8(a))
