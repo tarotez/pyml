a483abc717a79b0059907407a2a2890cc5672eec
tarotez <tez@sings.jp>
2019-10-18 12:20:40 +0900

    tensorboard

--- mnist_cnn_train.py
+++ mnist_cnn_train.py
@@ -68,7 +68,7 @@ model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
-          validation_data=(x_test, y_test)])
+          validation_data=(x_test, y_test))
 
 # save the model and its weights
 model.save_weights('model_weights_mnist_cnn.h5')
