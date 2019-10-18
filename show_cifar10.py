441ef4ebe86bdf4560004ebcce88aadc176447e2
tarotez <tez@sings.jp>
2019-10-18 11:36:53 +0900

    show images

--- show_cifar10.py
+++ show_cifar10.py
@@ -2,8 +2,8 @@ from PIL import Image
 import numpy as np
 from keras.datasets import cifar10
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
-for i in range(10):
+imgs = []
+for i in range(100):
 	a = X_train[i,:,:,:]
 	t = np.transpose(a,(1,2,0))
-	img = Image.fromarray(np.uint8(t))
-	img.show()
+	imgs.append(Image.fromarray(np.uint8(t)))
--- show_mnist.py
+++ show_mnist.py
@@ -2,7 +2,7 @@ from PIL import Image
 import numpy as np
 from keras.datasets import mnist
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
-for i in range(10):
-	a = X_train[i,:,:]
-	img = Image.fromarray(np.uint8(a))
-	img.show()
+imgs = []
+for i in range(100):
+  a = X_train[i,:,:]
+  imgs.append(Image.fromarray(np.uint8(a)))
