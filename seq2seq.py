3bfcf84441cce6b21a1d912c76f7599979ff7c6f
tarotez <tez@sings.jp>
2019-10-21 14:59:38 +0900

    neural style tranfer

--- seq2seq.py
+++ seq2seq.py
@@ -5,11 +5,11 @@ from keras.layers import Input, LSTM, Dense
 import numpy as np
 
 batch_size = 64  # Batch size for training.
-epochs = 10  # Number of epochs to train for.
+epochs = 5  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
 num_samples = 10000  # Number of samples to train on.
 # Path to the data txt file on disk.
-data_path = 'fra.txt'
+data_path = 'jpn.txt'
 
 # Vectorize the data.
 input_texts = []
