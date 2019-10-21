5aaeea0a2d48fe60a28dc13e2ed57690e90a0087
tarotez <tez@sings.jp>
2019-10-21 17:12:06 +0900

    picasso

--- seq2seq.py
+++ seq2seq.py
@@ -5,9 +5,9 @@ from keras.layers import Input, LSTM, Dense
 import numpy as np
 
 batch_size = 64  # Batch size for training.
-epochs = 5  # Number of epochs to train for.
+epochs = 10  # Number of epochs to train for.
 latent_dim = 256  # Latent dimensionality of the encoding space.
-num_samples = 10000  # Number of samples to train on.
+num_samples = 45000  # Number of samples to train on.
 # Path to the data txt file on disk.
 data_path = 'jpn.txt'
 
