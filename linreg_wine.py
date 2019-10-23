b15ea309267d7975e44edd3af516292d3706de2a
tarotez <tez@sings.jp>
2019-10-23 23:59:42 +0900

    wine

--- linreg_wine.py
+++ linreg_wine.py
@@ -5,6 +5,7 @@ from sklearn import linear_model
 
 d = pd.read_csv('winequality-red.csv', sep=';')
 indep_var_labels = ['fixed acidity']
+indep_targetID = 0
 dep_var_label = 'quality'
 indep = np.array([d[label] for label in indep_var_labels]).transpose()
 quality = d[dep_var_label].values
@@ -12,15 +13,14 @@ quality = d[dep_var_label].values
 lr = linear_model.LinearRegression()
 lr.fit(indep, quality)
 
-indep_test = np.linspace(indep.min(), indep.max(), len(indep))
-quality_predict = lr.predict(indep_test.reshape((-1,1)))
+indep_test = indep
+quality_predict = lr.predict(indep_test)
 fig, ax = plt.subplots()
-ax.scatter(indep, quality, label='observed', marker='*')
-ax.scatter(indep_test, quality_predict, c='green', marker='.' , label='prediction')
+ax.scatter(indep[:,indep_targetID], quality, c='blue', marker='.', label='observed')
+ax.scatter(indep_test[:,indep_targetID], quality_predict, c='red', marker='.', label='prediction')
 ax.legend(loc='lower left')
 
-targetID = 0
-ax.set_xlabel(indep_var_labels[targetID])
+ax.set_xlabel(indep_var_labels[indep_targetID])
 ax.set_ylabel(dep_var_label)
 
 error = quality - quality_predict
