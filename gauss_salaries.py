60b8e3747b166350d6c9af685a5c6bae6b3504b9
tarotez <tez@sings.jp>
2019-10-23 16:42:19 +0900

    salaries

---
+++ gauss_salaries.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+sal = pd.read_csv('net_salary_per_town_categories.csv')
+x = np.array(sal['SNHMC14'])
+bins = np.linspace(x.min(), x.max(), 100)
+h = plt.hist(x,bins)
+
+m = np.mean(x)
+s = np.std(x)
+sampleNum = x.shape[0]
+g = np.random.randn(sampleNum) * s + m
+bins = np.linspace(g.min(), g.max(), 100)
+h = plt.hist(x, bins, alpha=0.5, color='red')
+h = plt.hist(g, bins, alpha=0.5, color='blue')
